0644c4817dd1758cf9ac343e282fa37cfcf3d43c
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-03-01 13:40:35 +0200

    dd

--- src/const/routes.js
+++ src/const/routes.js
@@ -1,5 +1,5 @@
 export const HOME = "/";
-export const ABOUT = "about";
-export const PORTFOLIO = "portfolio";
-export const CONTACT = "contact";
-export const PROJECT = "portfolio/:id";
+export const ABOUT = "/about";
+export const PORTFOLIO = "/portfolio";
+export const CONTACT = "/contact";
+export const PROJECT = "/portfolio/:id";
